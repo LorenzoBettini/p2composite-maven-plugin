290aee57ff780d08aeb4c652a99bbdf33586ebf2
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-04-03 18:55:06 +0200

    ensure the output folder is cleaned in tests

--- p2composite-maven-plugin/src/test/java/io/github/lorenzobettini/p2utils/p2composite/P2CompositMojoMojoTest.java
+++ p2composite-maven-plugin/src/test/java/io/github/lorenzobettini/p2utils/p2composite/P2CompositMojoMojoTest.java
@@ -1,16 +1,19 @@
 package io.github.lorenzobettini.p2utils.p2composite;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class P2CompositMojoMojoTest {
+	private static final String TARGET_TEST_CLASSES = "target/test-classes/";
 	@Rule
 	public MojoRule rule = new MojoRule() {
 		@Override
@@ -39,14 +42,19 @@ public class P2CompositMojoMojoTest {
 	}
 
 	private P2CompositeMojo runMojo(File pom) throws Exception {
-		P2CompositeMojo myMojo = (P2CompositeMojo) rule.lookupConfiguredMojo(pom, "run");
+		P2CompositeMojo myMojo = rule.lookupConfiguredMojo(pom, "run");
 		assertNotNull(myMojo);
 		myMojo.execute();
 		return myMojo;
 	}
 
-	private File getPom(String projectPath) {
-		File pom = new File("target/test-classes/" + projectPath);
+	private File getPom(String projectPath) throws Exception {
+		var targetDirectory = new File(
+			TARGET_TEST_CLASSES + projectPath + "target");
+		FileUtils.deleteDirectory(targetDirectory);
+		assertThat(targetDirectory)
+			.doesNotExist();
+		File pom = new File(TARGET_TEST_CLASSES + projectPath);
 		assertNotNull(pom);
 		assertTrue(pom.exists());
 		return pom;
