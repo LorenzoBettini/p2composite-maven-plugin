a2ba96e6adaba4e91aa0195240efbe7485623456
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-04-07 19:25:43 +0200

    <extension>true</extension> in Tycho IT

--- p2composite-maven-plugin/src/it/copy-and-run-tycho-it/verify.groovy
+++ p2composite-maven-plugin/src/it/copy-and-run-tycho-it/verify.groovy
@@ -1,6 +1,6 @@
-File compositeArtifactsFile = new File( basedir, "composite/compositeArtifacts.jar" );
-File compositeContentFile = new File( basedir, "composite/compositeContent.jar" );
-File p2IndexFile = new File( basedir, "composite/p2.index" );
+File compositeArtifactsFile = new File( basedir, "site-project/composite/compositeArtifacts.jar" );
+File compositeContentFile = new File( basedir, "site-project/composite/compositeContent.jar" );
+File p2IndexFile = new File( basedir, "site-project/composite/p2.index" );
 
 assert compositeArtifactsFile.isFile()
 assert compositeContentFile.isFile()
