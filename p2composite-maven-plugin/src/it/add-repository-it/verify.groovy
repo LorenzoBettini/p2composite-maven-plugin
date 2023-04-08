ee98f2ffd8eba3546313fbab6446611ff10cc126
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-04-08 21:47:55 +0200

    started using BeanShell for verification

--- p2composite-maven-plugin/src/it/add-repository-it/verify.groovy
+++
@@ -1,7 +0,0 @@
-File compositeArtifactsFile = new File( basedir, "composite/compositeArtifacts.jar" );
-File compositeContentFile = new File( basedir, "composite/compositeContent.jar" );
-File p2IndexFile = new File( basedir, "composite/p2.index" );
-
-assert compositeArtifactsFile.isFile()
-assert compositeContentFile.isFile()
-assert p2IndexFile.isFile()
