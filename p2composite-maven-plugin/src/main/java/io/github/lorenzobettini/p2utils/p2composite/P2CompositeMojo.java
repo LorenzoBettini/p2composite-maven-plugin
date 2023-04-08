3e7b26cc07c42aeb5d9ba2e0ef7c286aea481b34
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-04-08 14:40:40 +0200

    extracted superclass

--- p2composite-maven-plugin/src/main/java/io/github/lorenzobettini/p2utils/p2composite/P2CompositeMojo.java
+++ p2composite-maven-plugin/src/main/java/io/github/lorenzobettini/p2utils/p2composite/P2CompositeMojo.java
@@ -1,14 +1,10 @@
 package io.github.lorenzobettini.p2utils.p2composite;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -22,32 +18,7 @@ import org.eclipse.equinox.p2.core.ProvisionException;
  * @author Lorenzo Bettini
  */
 @Mojo(name = "run", defaultPhase = LifecyclePhase.PACKAGE)
-public class P2CompositeMojo extends AbstractMojo {
-	/**
-	 * Location of the generated composite repository.
-	 */
-	@Parameter(defaultValue = "${project.build.directory}/compositerepo", property = "outputDir", required = true)
-	private File outputDirectory;
-
-	/**
-	 * Name for the composite repository.
-	 */
-	@Parameter(defaultValue = "Composite Artifact Repository")
-	private String name;
-
-	/**
-	 * Whether to compress the composite content/artifact xml into a jar.
-	 */
-	@Parameter(defaultValue = "false")
-	private boolean compressed;
-
-	/**
-	 * Whether the composite repository will fail to load if any of its children
-	 * fail to load.
-	 */
-	@Parameter(defaultValue = "false")
-	private boolean atomic;
-
+public class P2CompositeMojo extends AbstractP2CompositeMojo {
 	/**
 	 * List of repositories to add to the composite.
 	 */
@@ -59,9 +30,6 @@ public class P2CompositeMojo extends AbstractMojo {
 	 */
 	@Parameter
 	private List<String> childrenToRemove = new ArrayList<>();
-
-	@Inject
-	private CustomCompositeRepositoryApplication compositeRepositoryApplication;
 
 	public void execute() throws MojoExecutionException {
 		try {
